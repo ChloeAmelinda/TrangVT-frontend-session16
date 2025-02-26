a03f8a315f950226b13bfce42774558428865c37
ChloeAmelinda <trangthichngunuong@gmail.com>
2025-02-26 16:06:10 +0700

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,42 @@
+
+// // alert("Hello World1");
+// // alert("Hello World2");
+// // alert("Hello World3");
+
+// var fullname ="Tu sena";
+// alert(fullname);
+// var fullname ="Xemesis";
+// alert(fullname);
+// let age =35;
+
+// const dob ="1/1/2025";
+
+// // su dung bien
+// // fullname;
+// // age;
+// // dob;
+
+// let number1=10;
+// let number2= -10.5;
+// number1 + number2 ;
+// alert(number1 + number2);
+// let student1 = "Pham Thoai";
+// let student2 = "Be Bap";
+// let student3 =`Me be Bap`;
+// student1 + "sao ke"+ student3;
+// let result= `${student1} keu goi ung ho nhung ${student3} an chan het roi`;
+// alert(result);
+
+//  let realAge= prompt("Nhap tuoi :");
+
+
+//nhap vao 2 so 
+let number1= +prompt("Nhap so thu nhat :");
+let number2 = +prompt("Nhap so thu hai :");
+//tinh tong 2 so 
+ let result = number1 + number2;
+//in ra man hinh console
+console.log(result);
+alert(number1 + number2);
+
+
